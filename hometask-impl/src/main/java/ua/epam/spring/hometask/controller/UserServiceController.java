0c321aef34a02f0b2dc0e78116b137c2a3fdc217
Alexey Elnikov <Alexey_Elnikov@epam.com>
2019-04-03 12:17:30 +0300

    usercontroller refactoring and edit .gitignore

--- hometask-impl/src/main/java/ua/epam/spring/hometask/controller/UserServiceController.java
+++ hometask-impl/src/main/java/ua/epam/spring/hometask/controller/UserServiceController.java
@@ -1,8 +1,14 @@
 package ua.epam.spring.hometask.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import ua.epam.spring.hometask.domain.User;
 import ua.epam.spring.hometask.service.UserService;
 import java.util.Collection;
@@ -13,21 +19,19 @@ public class UserServiceController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/users", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/users")
     @ResponseBody
     public Collection<User> getAllUsers() {
         return userService.getAll();
     }
 
-    @RequestMapping(value = "/user/{userId}", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/user/{userId}")
     public User getUser(@PathVariable("userId") Long userId) {
         return userService.getById(userId);
     }
 
-    @RequestMapping(value = "/user/add", method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/user/add")
+    @ResponseStatus(HttpStatus.CREATED)
     public void addUser(@RequestBody User user) {
         userService.save(user);
     }
